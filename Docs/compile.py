ec08ec15720fec4c3ad8a17d52bde3c250c5859d
Mauricio Šmit <mauriciosmit100@gmail.com>
2022-08-17 22:26:55 +0200

    Html update

--- Docs/compile.py
+++ Docs/compile.py
@@ -32,7 +32,7 @@ def cleanName(fname : str) -> str:
 def safeName(name : str) -> str:
     return name.translate(name.maketrans(" ", "_")).lower()
 
-def getSource(path : str, level : int = 0) -> Page:
+def getSourceStructure(path : str, level : int = 0) -> Page:
     src = Page()
     src.name = cleanName(path)
     src.level = level
@@ -53,14 +53,14 @@ def getSource(path : str, level : int = 0) -> Page:
                 src.file = subpath
             # sub pages
             elif os.path.isdir(subpath) or subpath.endswith(".md"):
-                src.subPages[cleanName(subpath)] = getSource(subpath, level+1)
+                src.subPages[cleanName(subpath)] = getSourceStructure(subpath, level+1)
     else:
         src.file = path
         
 
     return src
 
-def getHtmlContents(src: Page, urlPrefix = "", index = 0, indent=0) -> str:
+def getHtmlContents(src: Page, urlPrefix = "", index = 0, indent=0, current="") -> str:
     ret = ""
     indentstr = 4*indent*" "
             
@@ -72,47 +72,60 @@ def getHtmlContents(src: Page, urlPrefix = "", index = 0, indent=0) -> str:
         if s in src.subPages.keys():
             subpage = src.subPages[s]
 
-            if subpage.file != "":
-                ret += indentstr+f'<li><a href="{urlPrefix+safeName(subpage.name)}.html">{str(i)} {subpage.name}</a>\n'
+            if subpage.name.lower() == current.lower():
+                ret += indentstr+f'<li><b>{str(i)} {subpage.name}</b>\n'
+            elif subpage.file != "":
+                ret += indentstr+f'<li><a href="{urlPrefix+safeName(subpage.name)}.html">{str(i)}&nbsp{subpage.name}</a>\n'
             else:
-                ret += indentstr+f'<li>{str(i)} {subpage.name}\n'
+                ret += indentstr+f'<li>{str(i)}&nbsp{subpage.name}\n'
 
             if len(subpage.order) > 0:
                 ret += indentstr+f'<ul>\n'
-                ret += getHtmlContents(subpage, urlPrefix+"", i, indent+1)
+                ret += getHtmlContents(subpage, urlPrefix+"", i, indent+1, current)
                 ret += indentstr+f'</ul>\n'
 
             ret += indentstr+f'</li>\n'
         else:
-            raise RuntimeError(f"subpage {s} doesn not exist")
+            raise RuntimeError(f"subpage {s} does not exist")
 
     return ret
 
-def toHtml(src: Page, current: Page, srcdir: str, targetdir: str):
-
-    # Get template
-    templ : str = ""
-    with open(srcdir+"/template.html") as file:
-        templ = file.read()
+def buildHtmlChunks(src: Page, currentPage: Page, srcdir: str, builddir: str):
     
     # Convert current file
-    if current.file != "":
-        result = subprocess.run(["pandoc", current.file, "--to", "html"], stdout=subprocess.PIPE)
-        pageContent : str = result.stdout.decode("utf8")
+    if currentPage.file != "":
+        outfilename = builddir+"/"+safeName(currentPage.name)+".html"
+        print(f"Checking chunk {outfilename}...")
+        if (not os.path.exists(outfilename)) or os.path.getmtime(currentPage.file) > os.path.getmtime(outfilename):
+            result = subprocess.run(["pandoc", currentPage.file, "--to", "html"], stdout=subprocess.PIPE)
+            pageContent : str = result.stdout.decode("utf8")
 
-        tableOfContents = getHtmlContents(src, index = 1, indent=2)
+            print(f"Generating chunk {outfilename}...")
+            with open(outfilename, "w+") as outfile:
+                outfile.write(pageContent)
+                print(f"Generated chunk {outfilename}")
+        else:
+            print(f"Generating not needed")
+
+    # convert sub pages
+    for name, subpage in currentPage.subPages.items():
+        buildHtmlChunks(src, subpage, srcdir, builddir)
+
+def buildHtmlFile(templ : str, src: Page, infilename: str, outfilename: str):
+    currentName = cleanName(infilename)
+    tableOfContents = getHtmlContents(src, index = 1, indent=2, current = currentName)
+
+    with open(infilename, "r") as file:
+        pageContent = file.read()
 
         htmlstr = templ
-        htmlstr = htmlstr.replace("(TITLE)", current.name)
+        htmlstr = htmlstr.replace("(TITLE)", currentName)
         htmlstr = htmlstr.replace("(MAIN_CONTENT)", pageContent)
         htmlstr = htmlstr.replace("(NAVIGATION_SIDEBAR)", tableOfContents)
 
-        with open(targetdir+"/"+safeName(current.name)+".html", "w") as outfile:
+        with open(outfilename, "w+") as outfile:
             outfile.write(htmlstr)
 
-    # convert sub pages
-    for name, subpage in current.subPages.items():
-        toHtml(src, subpage, srcdir, targetdir)
 
 
 
@@ -120,13 +133,45 @@ def main():
 
     srcdir = sys.argv[1]
     targetdir = sys.argv[2]
+    builddir = sys.argv[3]
     
-    src = getSource(srcdir, -1)
+    # get source
+    src = getSourceStructure(srcdir, -1)
 
-    print(json.dumps(src, indent=4, sort_keys=True, default=lambda o: getattr(o, '__dict__', str(o))))
-    print(getHtmlContents(src, index = 1))
+    # Get template
+    templ : str = ""
+    with open(srcdir+"/template.html") as file:
+        templ = file.read()
 
-    toHtml(src, src, srcdir, targetdir)
+    #print(json.dumps(src, indent=4, sort_keys=True, default=lambda o: getattr(o, '__dict__', str(o))))
+    #print(getHtmlContents(src, index = 1))
+
+    # Check if the file structure has changed
+    unselectedContents = getHtmlContents(src, index = 1, indent=2, current = "")
+    contentsUpdated = True
+    if os.path.exists(builddir+"/cached-contents.html"):
+        with open(builddir+"/cached-contents.html", "r") as file:
+            if file.read() == unselectedContents:
+                contentsUpdated = False
+    if contentsUpdated:
+        with open(builddir+"/cached-contents.html", "w+") as file:
+            file.write(unselectedContents)
+    
+    if contentsUpdated:
+        print("Contents changed, rebuilding all...")
+
+    # Build html files in /build dir
+    buildHtmlChunks(src, src, srcdir, builddir)
+
+    # Generate final html in target dir and copy needed files
+    for subname in os.listdir(builddir):
+        infilename = builddir + "/" + subname
+        if subname != "cached-contents.html" and not os.path.isdir(subname):
+            outfilename = targetdir + "/" + subname
+            if contentsUpdated or (not os.path.exists(outfilename)) or os.path.getmtime(infilename) > os.path.getmtime(outfilename):
+                print(f"Building {outfilename}")
+                buildHtmlFile(templ, src, infilename, outfilename)
+        
     shutil.copy2(srcdir+"/style.css", targetdir+"/style.css")
 
 
