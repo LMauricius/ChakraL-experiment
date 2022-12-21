16bd1d5d88d3dc269bef029014ab71f74fd86f93
LMauricius <mauriciosmit100@gmail.com>
2022-12-21 13:02:45 +0100

    File containing prereq declarations for the semantic node header

---
+++ Compiler/libChakraL/include/chakralSemanticsPrereq.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "model/chakralContext.h"
+#include "model/chakralValue.h"
+#include "model/chakralIdentifier.h"
+
+namespace ChakraL {
+
+    class Server;
+    class Identifier;
+    class Scope;
+}
